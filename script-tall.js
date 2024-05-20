3d2b59ecc50c0d10cf5cb9ebf8792b09b9fbb134
Javier Blanco <javierblancodetorres@gmail.com>
2024-05-20 16:44:11 +0700

    first commit

---
+++ script-tall.js
@@ -0,0 +1,39 @@
+var thirdOfAMeter = 2;
+var tall = 3;
+
+function addMoreContent() {
+  const tallCounter = document.getElementById('tall-counter');
+  const preElement = document.getElementById('tall-content');
+  let moreText = '';
+  for (let i = 0; i < 10; i++) {
+    if (i % 30 == 0) {
+      if (thirdOfAMeter == 2) {
+        if (tall == 9999) {
+          var howTall = 'OMG!';
+          document.title = 'OMG! Too tall!!';
+        } else {
+          var howTall = tall.toString().padStart(4, '0') + 'm';
+          document.title = '👨🏻 Taller than ' + tall + 'm!';
+          tall += 1;
+        }
+        moreText += '\n    <span class="color-white">|</span> ' + '<span class="color-white">' + howTall + '</span>' + ' <span class="color-white">|</span><span class="color-white">.</span>                      <span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span>  <span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span>                    ';
+        thirdOfAMeter = 0;
+      } else {
+        thirdOfAMeter += 1;
+      }
+    } else {
+      moreText += '\n    <span class="color-white">|</span>       <span class="color-white">|</span><span class="color-white">.</span>                      <span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span>  <span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span><span class="no-color">▒▒</span>                    ';
+    }
+  }
+  preElement.innerHTML += moreText;
+  tallCounter.innerHTML = tall;
+}
+
+function checkScroll() {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    addMoreContent();
+  }
+}
+
+window.addEventListener('scroll', checkScroll);
+addMoreContent();
